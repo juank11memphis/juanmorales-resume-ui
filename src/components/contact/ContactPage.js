f75459a10a7e287168503a98372041234f392bc3
Juan Morales <juank.memphis@gmail.com>
2017-02-25 07:27:19 -0600

    Adds the empty version of all possible pages

---
+++ src/components/contact/ContactPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+class ContactPage extends React.Component {
+
+  render() {
+    return (
+      <Container>
+        Contact page is gonna be here very very very soon....
+      </Container>
+    );
+  }
+
+}
+
+export default ContactPage;
--- src/core/routes.js
+++ src/core/routes.js
@@ -3,9 +3,13 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from '../components/App';
 import { HomePage } from '../components/home';
+import { ProjectsPage } from '../components/projects';
+import { ContactPage } from '../components/contact';
 
 export default (
   <Route path="/" component={App} >
     <IndexRoute component={HomePage} />
+    <Route path="projects" component={ProjectsPage} />
+    <Route path="contact" component={ContactPage} />
   </Route>
 );
